2d48b9598546de06ca9b70662768d7cab2004089
Max Squirrel <maxbelka888@gmail.com>
2020-10-06 19:00:07 +0300

    WIP: attach file

--- src/taskpane/app/app.component.ts
+++ src/taskpane/app/app.component.ts
@@ -1,52 +1,27 @@
 import { Component } from "@angular/core";
-import { P } from "@angular/core/src/render3";
+
 // images references in the manifest
 import "../../../assets/icon-16.png";
 import "../../../assets/icon-32.png";
 import "../../../assets/icon-80.png";
-import pptxgen from "pptxgenjs";
 const template = require("./app.component.html");
 /* global console, Office, require */
+/* global PowerPoint*/
 
 @Component({
   selector: "app-home",
   template
 })
 export default class AppComponent {
-  welcomeMessage = "Hi!";
-  color : string;
 
-  countNext: number = 0;
-  countPrev: number = 0;
+ async AddFile(){
+  var reader = new window.FileReader();
+  reader.onload = function (event) {
+    // strip off the metadata before the base64-encoded string
+    var startIndex = (event.target.result as string).indexOf("base64,");
+    var copyBase64 = (event.target.result as string).substr(startIndex + 7);
+    Office.context.document.setSelectedDataAsync(copyBase64);    
+  };
+ }
 
-  async goToFirstSlide() {
-    var goToFirst = Office.Index.First;
-
-    Office.context.document.goToByIdAsync(goToFirst, Office.GoToType.Index);
-  }
-
-  async goToLastSlide() {
-    var goToLast = Office.Index.Last;
-
-    Office.context.document.goToByIdAsync(goToLast, Office.GoToType.Index);
-  }
-
-  async goToNextSlide() {
-    var goToNext = Office.Index.Next;
-
-    Office.context.document.goToByIdAsync(goToNext, Office.GoToType.Index);
-    this.countNext++;
-  }
-
-  async goToPrevSlide() {
-    var goToPrev = Office.Index.Previous;
-
-    Office.context.document.goToByIdAsync(goToPrev, Office.GoToType.Index);
-    this.countPrev++;
-  }
-
-  async changeColor(){
-   var random = Math.floor(Math.random() * 2);
-   return random == 0 ? this.color = "green" : this.color = "red";
-  }
 }
