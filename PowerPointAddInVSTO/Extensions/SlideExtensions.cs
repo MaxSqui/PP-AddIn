56f95b8898b3a5b21e694078a75d94abc153eb8a
Ihor Panasiuk <ihor.panasiuk@nixsolutions.com>
2020-10-12 20:47:17 +0300

    Add slide extensions

---
+++ PowerPointAddInVSTO/Extensions/SlideExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Microsoft.Office.Interop.PowerPoint;
+
+namespace PowerPointAddInVSTO.Extensions
+{
+    public static class SlideExtensions
+    {
+        public static IEnumerable<Shape> GetAnimatedObjects(this Slide slide)
+        {
+            foreach(Shape shape in slide.Shapes)
+            {
+                if(shape.AnimationSettings.Animate == Microsoft.Office.Core.MsoTriState.msoTrue)
+                {
+                    yield return shape;
+                }
+            }
+        }
+    }
+}
