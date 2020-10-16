e0d76ced25a991354c3059e2d2845e6384c5392e
Max Squirrel <maxbelka888@gmail.com>
2020-10-16 18:33:52 +0300

    UI to insert audio to each slide

--- PowerPointAddInVSTO/ThisAddIn.cs
+++ PowerPointAddInVSTO/ThisAddIn.cs
@@ -9,24 +9,19 @@ using System.Windows.Forms;
 using Microsoft.Office.Interop.PowerPoint;
 using Microsoft.Office.Core;
 using Shape = Microsoft.Office.Interop.PowerPoint.Shape;
+using PowerPointAddInVSTO.Extensions;
+using PowerPointAddInVSTO.UI;
+using Application = Microsoft.Office.Interop.PowerPoint.Application;
+using System.Collections.ObjectModel;
 
 namespace PowerPointAddInVSTO
 {
     public partial class ThisAddIn
     {
-        void Application_PresentationNewSlide(PowerPoint.Slide Sld, float X, float Y)
-        {
-            int a = Sld.TimeLine.InteractiveSequences.Count;
-            PowerPoint.Shape textBox = Sld.Shapes.AddTextbox(
-                Office.MsoTextOrientation.msoTextOrientationHorizontal, 0, 0, 500, 50);
-            textBox.TextFrame.TextRange.InsertAfter("This text was added by using code.");
-        }
 
         private void ThisAddIn_Startup(object sender, System.EventArgs e)
         {
-            var myUserControl1 = new UserControl();
-            var myCustomTaskPane = this.CustomTaskPanes.Add(myUserControl1, "My Task Pane");
-            myCustomTaskPane.Visible = true;
+
         }
 
         private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
@@ -44,62 +39,53 @@ namespace PowerPointAddInVSTO
         {
             this.Startup += new System.EventHandler(ThisAddIn_Startup);
             this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
-            this.Application.AfterDragDropOnSlide += Application_PresentationNewSlide;
 
             this.Application.PresentationNewSlide += Application_PresentationNewSlide1;
 
-            this.Application.AfterShapeSizeChange += Application_AfterShapeSizeChange;
-
-            this.Application.WindowSelectionChange += Application_WindowSelectionChange;
-
-            this.Application.WindowDeactivate += Application_WindowDeactivate; ;
-
         }
 
-        private void Application_WindowDeactivate(Presentation Pres, DocumentWindow Wn)
+        public List<Slide> GetSlides()
         {
+            List<Slide> slides = new List<Slide>();
 
+            foreach (Slide slide in this.Application.ActivePresentation.Slides)
+            {
+                slides.Add(slide);
+            }
+            return slides;
         }
-
-        private void Application_WindowSelectionChange(PowerPoint.Selection Sel)
-        {
-        }
-
-        private void Application_AfterShapeSizeChange(PowerPoint.Shape shp)
-        {
-
-        }
-
         private void Application_PresentationNewSlide1(Slide Sld)
         {
+            var a = new AudioInserter();
+            a.Show();
             Shape textBox = Sld.Shapes.AddTextbox(
                Office.MsoTextOrientation.msoTextOrientationHorizontal, 0, 0, 500, 50);
             textBox.TextFrame.TextRange.InsertAfter("This text was added by using code.");
 
-            string path = "C:/Users/maxbe/Downloads/Lil_Uzi_Vert-Baby_Pluto.mp3";
-            //add audio
-            Shape audio = Sld.Shapes.AddMediaObject2(path, MsoTriState.msoTrue);
+            //string path = "C:/Users/maxbe/Downloads/Lil_Uzi_Vert-Baby_Pluto.mp3";
+            ////add audio
+            //Shape audio = Sld.Shapes.AddMediaObject2(path, MsoTriState.msoTrue);
 
-            //add settings to the audio (automatic play)
-            Sld.TimeLine.MainSequence.AddEffect(audio, MsoAnimEffect.msoAnimEffectMediaPlay, MsoAnimateByLevel.msoAnimateLevelNone ,MsoAnimTriggerType.msoAnimTriggerWithPrevious);
-            audio.AnimationSettings.PlaySettings.PlayOnEntry = MsoTriState.msoTrue;
-            audio.AnimationSettings.Animate = MsoTriState.msoTrue;
-            audio.AnimationSettings.PlaySettings.LoopUntilStopped = MsoTriState.msoTrue;
-            audio.AnimationSettings.PlaySettings.HideWhileNotPlaying = MsoTriState.msoTrue;
+            ////add settings to the audio (automatic play)
+            //Sld.TimeLine.MainSequence.AddEffect(audio, MsoAnimEffect.msoAnimEffectMediaPlay, MsoAnimateByLevel.msoAnimateLevelNone ,MsoAnimTriggerType.msoAnimTriggerWithPrevious);
+            //audio.AnimationSettings.PlaySettings.PlayOnEntry = MsoTriState.msoTrue;
+            //audio.AnimationSettings.Animate = MsoTriState.msoTrue;
+            //audio.AnimationSettings.PlaySettings.LoopUntilStopped = MsoTriState.msoTrue;
+            //audio.AnimationSettings.PlaySettings.HideWhileNotPlaying = MsoTriState.msoTrue;
 
             //add bookmark = duration-value (ms) & name
-            Sequence audioSequence = Sld.TimeLine.InteractiveSequences.Add();
-            audio.MediaFormat.MediaBookmarks.Add(5000, "yeet");
+            //Sequence audioSequence = Sld.TimeLine.InteractiveSequences.Add();
+            //audio.MediaFormat.MediaBookmarks.Add(5000, "yeet");
 
             //creating new shape and bind with exist bookmark
-            Shape rectangle = Sld.Shapes.AddShape(MsoAutoShapeType.msoShapeRoundedRectangle, 200, 160, 100, 50);
-            Sequence objSequence = Sld.TimeLine.InteractiveSequences.Add();
-            objSequence.AddTriggerEffect(rectangle, MsoAnimEffect.msoAnimEffectZoom, MsoAnimTriggerType.msoAnimTriggerOnMediaBookmark, audio, "yeet");
+            //Shape rectangle = Sld.Shapes.AddShape(MsoAutoShapeType.msoShapeRoundedRectangle, 200, 160, 100, 50);
+            //Sequence objSequence = Sld.TimeLine.InteractiveSequences.Add();
+            //objSequence.AddTriggerEffect(rectangle, MsoAnimEffect.msoAnimEffectZoom, MsoAnimTriggerType.msoAnimTriggerOnMediaBookmark, audio, "yeet");
         }
 
         //separate methods (WIP)
 
-        private void SetAudio(Slide Sld, string path)
+        public void SetAudio(Slide Sld, string path)
         {
             Shape audio = Sld.Shapes.AddMediaObject2(path, MsoTriState.msoTrue);
 
@@ -120,7 +106,7 @@ namespace PowerPointAddInVSTO
             audio.MediaFormat.MediaBookmarks.Add((int)durationTime, bookMarkName);
         }
 
-        private void BindShapeToBookmark(Slide Sld, Shape shape, Shape audio, MediaBookmark bookmark)
+        private void TriggerShapeToBookmark(Slide Sld, Shape shape, Shape audio, MediaBookmark bookmark)
         {
             Sequence objSequence = Sld.TimeLine.InteractiveSequences.Add();
             objSequence.AddTriggerEffect(shape, MsoAnimEffect.msoAnimEffectZoom, MsoAnimTriggerType.msoAnimTriggerOnMediaBookmark, audio, bookmark.Name);
---
+++ PowerPointAddInVSTO/UI/AudioInserter.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Office.Interop.PowerPoint;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PowerPointAddInVSTO.UI
+{
+    /// <summary>
+    /// Interaction logic for AudioInserter.xaml
+    /// </summary>
+    public partial class AudioInserter : Window
+    {
+        public AudioInserter()
+        {
+            InitializeComponent();
+
+            var addIn = Globals.ThisAddIn;
+
+            slidetrack.ItemsSource = addIn.GetSlides();
+        }
+
+        public string FilePath { get; private set; }
+
+        private void Browse(object sender, RoutedEventArgs e)
+        {
+            var addIn = Globals.ThisAddIn;
+            var currentSlide = (Slide)slidetrack.Items.CurrentItem;
+
+            Microsoft.Win32.OpenFileDialog openFileDlg = new Microsoft.Win32.OpenFileDialog();
+            if (openFileDlg.ShowDialog() == true)
+            {
+                this.FilePath = openFileDlg.FileName;
+                addIn.SetAudio(currentSlide, FilePath);
+            }
+        }
+
+    }
+}
